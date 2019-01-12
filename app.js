82c7c9246ca7ee781172b1b8e5c1d3ccdf351869
gowthamgenupur <genupur.g@husky.neu.edu>
2019-01-12 11:26:19 -0500

    Configured session storage using connect-mongo

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var session = require('express-session');
 var passport = require('passport');
 var flash = require('connect-flash');
 var validator = require('express-validator');
+var MongoStore = require('connect-mongo')(session);
 
 var indexRoutes = require('./routes/index');
 var userRoutes = require('./routes/user');
@@ -37,7 +38,15 @@ app.use(cookieParser());
 //on the server.
 
 //below line sets up the session.
-app.use(session({secret: 'mysupersecret', resave: false, saveUninitialized: false}));
+app.use(session({
+  secret: 'mysupersecret', 
+  resave: false, 
+  saveUninitialized: false,
+  //Using the existing mongoose connection.
+  store: new MongoStore({ mongooseConnection: mongoose.connection}),
+  //Setting how long cookie must exist in milliseconds.
+  cookie: {maxAge: 180 * 60 * 1000}
+}));
 
 //session needs to be initialised before flash.
 app.use(flash());
@@ -49,6 +58,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(function(req, res, next) {
   //this is a global variable that is visible to all the files.
   res.locals.login = req.isAuthenticated();
+  res.locals.session = req.session;
   next();
 });
 
